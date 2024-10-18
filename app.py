8c2435f75b9f90d3abbe357b24275af186d3b80f
Amalesh Fernando Jesu Thomas <amaleshj.in@mouritech.com>
2024-10-18 13:22:19 +0530

    port added

--- app.py
+++ app.py
@@ -132,6 +132,6 @@ APP.router.add_post("/api/messages", messages)
 
 if __name__ == "__main__":
     try:
-        web.run_app(APP)
+        web.run_app(APP, port=CONFIG.PORT)
     except Exception as error:
         raise error
